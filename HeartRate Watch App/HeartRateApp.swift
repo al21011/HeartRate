4bc0645fda941e5c46af0984cd3dadb0b4a55fcc
ryota <al21011@shibaura-it.ac.jp>
2024-07-20 14:36:45 +0900

    Initial Commit

---
+++ HeartRate Watch App/HeartRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateApp.swift
+//  HeartRate Watch App
+//
+//  Created by ryota on 2024/07/20.
+//
+
+import SwiftUI
+
+@main
+struct HeartRate_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HeartRate/HeartRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateApp.swift
+//  HeartRate
+//
+//  Created by ryota on 2024/07/20.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
