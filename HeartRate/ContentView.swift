6c1b5200d5c6483f321959942ec682f1a4d4f151
ryota <al21011@shibaura-it.ac.jp>
2024-07-25 02:31:09 +0900

    アプリ内での心拍数表示は完成。

--- HeartRate/ContentView.swift
+++ HeartRate/ContentView.swift
@@ -1,24 +1,45 @@
-//
-//  ContentView.swift
-//  HeartRate
-//
-//  Created by ryota on 2024/07/20.
-//
-
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
+    // healthKit認証関連
+    @State private var labelText = "HealthKit Authorization"
+    @State private var flag = false
+    let healthStore = HKHealthStore()
+    let allTypes = Set([HKObjectType.quantityType(forIdentifier: .heartRate)!])
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(labelText)
+                .font(.title)
+            Button(action: {
+                if(self.flag) {
+                    self.flag = false
+                } else {
+                    if HKHealthStore.isHealthDataAvailable() {
+                        self.labelText = "Available"
+                        self.healthStore.requestAuthorization(toShare: nil, read: self.allTypes){ (success, error) in}
+                    } else {
+                        self.labelText = "Unavailable"
+                    }
+                    self.flag = true
+                }
+            }){
+                Text("Access")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+            }
+            .padding(.all)
+            .background(Color.blue)
+            
+            Spacer()
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
